b60abccebfd53dc3133acd326c5e3f3aa324fddd
zhenkondrat <zhenkondrat@meta.ua>
2018-12-11 23:30:30 +0200

    fix parse and insert to db

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 require_once("vendor/autoload.php");
 
-// $host = 'localhost'; 
-// $user = 'root';
-// $password = ''; 
-// $db_name = 'parser_db'; 
+/*data basse connection*/
+$host = 'localhost'; 
+$user = 'root';
+$password = ''; 
+$db_name = 'parser_db'; 
 
-// $conn = mysqli_connect($host, $user, $password, $db_name);
-// mysqli_query($link, "SET NAMES 'utf8'");
+$conn = mysqli_connect($host, $user, $password, $db_name);
+mysqli_query($link, "SET NAMES 'utf8'");
 
 
 function searchForSymb($id, $array) {
@@ -50,7 +51,6 @@ function from_preparaty_org(){
 	phpQuery::unloadDocuments();
 	return $tmp;
 }
-
 //$tmp = from_preparaty_org();
 
 /*coompedium*/
@@ -60,13 +60,13 @@ function data_from_compendium_page($url, &$arr, $parent_id, $is_first_request){
 	$host = "https://compendium.com.ua";
 	$html = file_get_contents($url);
 	$d = phpQuery::newDocument($html);
-	$id = count($arr)+1;
+	$id = -1;
 	$flag = false;
 	$tmp = array();
 
 	$selector = $is_first_request == true ? 'p>a' : '.list-unstyled:first li>a';
 
-	foreach($d->find($selector) as $link){ //'.list-unstyled:first li>a'
+	foreach($d->find($selector) as $link){
 		$flag = true;
 		$b = pq($link)->find("b:first");
 		$a = pq($link);
@@ -78,18 +78,19 @@ function data_from_compendium_page($url, &$arr, $parent_id, $is_first_request){
 			"text" => $a->text(),
 			"url"  => $a->attr('href')
 		);	
-		$id++;
 	}
 
 	foreach($tmp as $item){
+		$item['id'] = count($arr)+1;
 		array_push($arr, $item);
-	//	mysqli_query($conn, "INSERT INTO `atx`(`id`, `text`, `symb`, `parent_id`) VALUES (" . ($item['id']+8000) . ",'" . $item['text'] . "','" . $item['symb'] . "'," . ($item['parent'] + +8000) .")");
+		mysqli_query($conn, "INSERT INTO `atx`(`id`, `title`, `category_id`) VALUES (" . $item['id'] . ",'" . $item['text'] . "'," . $item['parent'] .")");
 		data_from_compendium_page($host. $item['url'], $arr, $item['id'], false);		
 	}
 	
 	phpQuery::unloadDocuments();
 }
 
+/*main coompedium function*/
 function from_compendium(){
 	$host = "https://compendium.com.ua";
 	$tmp = array();
@@ -101,13 +102,15 @@ function from_compendium(){
 	return $tmp;
 }
 $tmp = from_compendium();
+
+/* parse begin 21:31 - end 22:11*/
 ?>
 
 <ul>
 	<?php foreach($tmp as $value): ?>
 		<li>
 			<a href="<?php echo($value["url"]); ?>" target="_blank">
-				<b><?php echo($value["symb"]);?> </b>  <?php echo($value["id"]);?>[<?php echo($value["parent"]);?>]	<?php echo($value["text"]); ?> 
+				<b><?php echo($value["symb"]);?> </b>    <?php echo($value["id"]);?>[<?php echo($value["parent"]);?>]      	<?php echo($value["text"]); ?> 
 			</a>
 		</li>
 	<?php endforeach; ?>
